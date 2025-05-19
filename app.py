61aa1887f3933c44432a8890cdd7c0b792688a07
Kyle Lee <kylelee4c6@gmail.com>
2025-05-19 17:04:52 -0400

    Took out main code

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 
 def main():
-    st.switch_page("pages/landing.py")
+    #st.switch_page("pages/landing.py")
+    st.write("The app is currently down at the moment.")
 
 if __name__ == "__main__":
     main()

81ec0dd235178045362a0182cc353cda7e517412
Kyle Lee <kylelee4c6@gmail.com>
2025-06-30 21:14:10 -0400

    Modded page config

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-st.set_page_config(layout = "wide")
+
 def main():
-    
+    st.set_page_config(layout = "wide")
     st.switch_page("pages/landing.py")
     #st.write("The app is currently down at the moment.")
 
